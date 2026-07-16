aeceb72155d4015996dc834690065a5d64e32421
GameTec_live <benedikt.werner08@gmail.com>
2026-07-16 14:40:34 +0200

    feat: openapi

--- apps/api/src/auth.ts
+++ apps/api/src/auth.ts
@@ -14,3 +14,27 @@ export const auth = betterAuth({
         enabled: true,
     },
 });
+
+let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>;
+const getSchema = async () => (_schema ??= auth.api.generateOpenAPISchema());
+export const OpenAPI = {
+    getPaths: (prefix = "/api/auth") =>
+        getSchema().then(({ paths }) => {
+            const reference: typeof paths = Object.create(null);
+            for (const path of Object.keys(paths)) {
+                const pathDefinition = paths[path];
+                if (!pathDefinition) continue;
+
+                const key = prefix + path;
+                reference[key] = pathDefinition;
+                for (const method of Object.keys(pathDefinition)) {
+                    const operation = (reference[key] as any)[method];
+                    operation.tags = ["Better Auth"];
+                }
+            }
+            return reference;
+        }) as Promise<any>,
+    components: getSchema().then(
+        ({ components }) => components,
+    ) as Promise<any>,
+} as const;
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,36 @@
+import { fromTypes, openapi } from "@elysia/openapi";
 import { Elysia } from "elysia";
-import { db } from "./db";
-import { vehicle } from "./db/schema";
+import { OpenAPI } from "./auth";
+import { authHandler } from "./authHandler";
 
-const app = new Elysia()
-    .get("/", async () => {
-        const data = await db.select().from(vehicle);
-        return data;
-    })
+export const app = new Elysia()
+    .use(
+        openapi({
+            references: fromTypes("src/index.ts", {
+                tmpRoot: "./.cache/elysia-openapi",
+            }),
+            documentation: {
+                components: await OpenAPI.components,
+                paths: await OpenAPI.getPaths(),
+            },
+        }),
+    )
+    .use(authHandler)
+    .get("/", () => "Atlas API")
     .listen(3000);
 
+const banner = `
+       d8888 888    888
+      d88888 888    888
+     d88P888 888    888
+    d88P 888 888888 888  8888b.  .d8888b
+   d88P  888 888    888     "88b 88K
+  d88P   888 888    888 .d888888 "Y8888b.
+ d8888888888 Y88b.  888 888  888      X88
+d88P     888  "Y888 888 "Y888888  88888P' 
+`;
+
+console.log(`Thank you for trusting\n${banner}`);
 console.log(
-    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+    `🌐 Atlas API is running at http://${app.server?.hostname}:${app.server?.port}`,
 );
